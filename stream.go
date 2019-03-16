7fa95f20aca46e13b076331462baf7d0bcf53bc1
Austin Ward <ward.austin28@gmail.com>
2019-03-16 12:34:38 -0400

    Implementing skip, limit and generate

--- stream.go
+++ stream.go
@@ -1,7 +1,6 @@
 package stream
 
 import (
-	"fmt"
 	op "github.com/MercuryThePlanet/optional"
 	"runtime"
 )
@@ -10,70 +9,74 @@ var (
 	CORES = runtime.NumCPU()
 )
 
-func T() {
-	fmt.Println("vim-go")
-}
-
 type Spliterator interface {
-	TryAdvance(op.Consumer) bool
-	ForEachRemaining(op.Consumer)
+	TryAdvance(Consumer) bool
+	ForEachRemaining(Consumer)
 	TrySplit() Spliterator
-}
-
-type Array struct {
-	ts  op.Ts
-	idx int
-}
-
-func NewArray(ts op.Ts) *Array {
-	return &Array{ts, 0}
-}
-
-func (a *Array) TryAdvance(c op.Consumer) bool {
-	if a.idx >= len(a.ts) {
-		return false
-	}
-	c(a.ts[a.idx])
-	a.idx++
-	return true
-}
-
-func (a *Array) ForEachRemaining(c op.Consumer) {
-	for a.TryAdvance(c) {
-	}
-}
-
-func (a *Array) TrySplit() (s Spliterator) {
-	remain := len(a.ts) - a.idx
-	if remain > 1 {
-		var split int = (remain) / 2
-		s = &Array{ts: a.ts[split:], idx: 0}
-		a.ts = a.ts[:split]
-		return
-	}
-	return nil
 }
 
 type (
 	Mapper    func(op.T) op.T
+	Supplier  func() op.T
 	Consumer  func(op.T)
 	Predicate func(op.T) bool
 )
 
 type Stream struct {
-	pipeline func(op.T, Consumer)
-	spltr    Spliterator
+	pipeline     func(op.T, Consumer)
+	spltr        Spliterator
+	limitReached bool
+}
+type generator struct {
+	supplier Supplier
+}
+
+func (g *generator) TryAdvance(c Consumer) bool {
+	c(g.supplier())
+	return true
+}
+
+func (g *generator) ForEachRemaining(c Consumer) {
+	for {
+		c(g.supplier())
+	}
+}
+
+func (g *generator) TrySplit() Spliterator {
+	return nil
 }
 
 func Of(s Spliterator) *Stream {
-	return &Stream{func(t op.T, c Consumer) { c(t) }, s}
+	return &Stream{func(t op.T, c Consumer) { c(t) }, s, false}
+}
+
+func Generate(supplier Supplier) *Stream {
+	return Of(&generator{supplier})
+}
+
+func (s *Stream) Skip(num int) *Stream {
+	cur := s.pipeline
+	skipped := 0
+	s.pipeline = func(t op.T, c Consumer) {
+		cur(t, func(t op.T) {
+			if skipped >= num {
+				c(t)
+			} else {
+				skipped += 1
+			}
+		})
+	}
+	return s
 }
 
 func (s *Stream) Limit(size int) *Stream {
 	cur := s.pipeline
+	limit := 0
 	s.pipeline = func(t op.T, c Consumer) {
 		cur(t, func(t op.T) {
 			c(t)
+			limit += 1
+			s.limitReached = limit >= size
 		})
 	}
 	return s
@@ -107,7 +110,7 @@ func (s *Stream) AllMatch(p Predicate) (matches bool) {
 		s.pipeline(t, func(t op.T) {
 			matches = p(t)
 		})
-	}) {
+	}) && !s.limitReached {
 		if !matches {
 			break
 		}
@@ -124,7 +127,7 @@ func (s *Stream) AnyMatch(p Predicate) bool {
 			s.pipeline(t, func(t op.T) {
 				found = true
 			})
-		}) {
+		}) && !s.limitReached {
 			if found {
 				break
 			}
@@ -146,7 +149,7 @@ func (s *Stream) AnyMatch(p Predicate) bool {
 func (s *Stream) Count() (count int) {
 	for s.spltr.TryAdvance(func(t op.T) {
 		s.pipeline(t, func(t op.T) {})
-	}) {
+	}) && !s.limitReached {
 		count++
 	}
 	return
@@ -155,7 +158,7 @@ func (s *Stream) Count() (count int) {
 func (s *Stream) ForEach(c Consumer) {
 	for s.spltr.TryAdvance(func(t op.T) {
 		s.pipeline(t, c)
-	}) {
+	}) && !s.limitReached {
 	}
 }
 
@@ -169,7 +172,7 @@ func (s *Stream) FindAny() *op.Optional {
 				rv_chan <- op.Of(t)
 				found = true
 			})
-		}) {
+		}) && !s.limitReached {
 			if found {
 				break
 			}
--- stream_test.go
+++ stream_test.go
@@ -6,9 +6,43 @@ import (
 	"testing"
 )
 
+type Array struct {
+	ts  op.Ts
+	idx int
+}
+
+func NewArray(ts op.Ts) *Array {
+	return &Array{ts, 0}
+}
+
+func (a *Array) TryAdvance(c stream.Consumer) bool {
+	if a.idx >= len(a.ts) {
+		return false
+	}
+	c(a.ts[a.idx])
+	a.idx++
+	return true
+}
+
+func (a *Array) ForEachRemaining(c stream.Consumer) {
+	for a.TryAdvance(c) {
+	}
+}
+
+func (a *Array) TrySplit() (s stream.Spliterator) {
+	remain := len(a.ts) - a.idx
+	if remain > 1 {
+		var split int = (remain) / 2
+		s = &Array{ts: a.ts[split:], idx: 0}
+		a.ts = a.ts[:split]
+		return
+	}
+	return nil
+}
+
 func TestStream(t *testing.T) {
-	gen := func() *stream.Array {
-		return stream.NewArray(makeRange(1, 10))
+	gen := func() *Array {
+		return NewArray(makeRange(1, 10))
 	}
 
 	stream.Of(gen()).Map(func(t op.T) op.T {
@@ -21,6 +55,19 @@ func TestStream(t *testing.T) {
 		return t.(int) > 1
 	})
 	println(matches)
+
+	stream.Of(gen()).Limit(10).ForEach(func(t op.T) {
+		println(t.(int))
+	})
+
+	i := -1
+	supplier := func() op.T {
+		i++
+		return i
+	}
+	stream.Generate(supplier).Skip(10).Limit(10).Skip(5).ForEach(func(t op.T) {
+		println(t.(int))
+	})
 }
 
 func makeRange(min, max int) []interface{} {
