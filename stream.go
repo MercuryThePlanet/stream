27802d33bfc9cfee1e547656203fb0f544e719a1
Austin Ward <ward.austin28@gmail.com>
2019-03-16 11:51:47 -0400

    Adding some methods

--- stream.go
+++ stream.go
@@ -3,6 +3,11 @@ package stream
 import (
 	"fmt"
 	op "github.com/MercuryThePlanet/optional"
+	"runtime"
+)
+
+var (
+	CORES = runtime.NumCPU()
 )
 
 func T() {
@@ -64,6 +69,16 @@ func Of(s Spliterator) *Stream {
 	return &Stream{func(t op.T, c Consumer) { c(t) }, s}
 }
 
+func (s *Stream) Limit(size int) *Stream {
+	cur := s.pipeline
+	s.pipeline = func(t op.T, c Consumer) {
+		cur(t, func(t op.T) {
+			c(t)
+		})
+	}
+	return s
+}
+
 func (s *Stream) Filter(p Predicate) *Stream {
 	cur := s.pipeline
 	s.pipeline = func(t op.T, c Consumer) {
@@ -86,9 +101,108 @@ func (s *Stream) Map(m Mapper) *Stream {
 	return s
 }
 
+func (s *Stream) AllMatch(p Predicate) (matches bool) {
+	matches = true
+	for s.spltr.TryAdvance(func(t op.T) {
+		s.pipeline(t, func(t op.T) {
+			matches = p(t)
+		})
+	}) {
+		if !matches {
+			break
+		}
+	}
+	return
+}
+
+func (s *Stream) AnyMatch(p Predicate) bool {
+	rv_chan := make(chan bool)
+
+	find := func(spltr Spliterator) {
+		found := false
+		for spltr.TryAdvance(func(t op.T) {
+			s.pipeline(t, func(t op.T) {
+				found = true
+			})
+		}) {
+			if found {
+				break
+			}
+		}
+		rv_chan <- found
+	}
+
+	splits := split(s.spltr, CORES, find)
+
+	for i := 0; i < splits; i++ {
+		if <-rv_chan {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (s *Stream) Count() (count int) {
+	for s.spltr.TryAdvance(func(t op.T) {
+		s.pipeline(t, func(t op.T) {})
+	}) {
+		count++
+	}
+	return
+}
+
 func (s *Stream) ForEach(c Consumer) {
 	for s.spltr.TryAdvance(func(t op.T) {
 		s.pipeline(t, c)
 	}) {
+	}
+}
+
+func (s *Stream) FindAny() *op.Optional {
+	rv_chan := make(chan *op.Optional)
+
+	find := func(spltr Spliterator) {
+		found := false
+		for spltr.TryAdvance(func(t op.T) {
+			s.pipeline(t, func(t op.T) {
+				rv_chan <- op.Of(t)
+				found = true
+			})
+		}) {
+			if found {
+				break
+			}
+		}
+		if !found {
+			rv_chan <- op.Empty()
+		}
+	}
+
+	splits := split(s.spltr, CORES, find)
+
+	for i := 0; i < splits; i++ {
+		o := <-rv_chan
+		if o.IsPresent() {
+			return o
+		}
+	}
+
+	return op.Empty()
+}
+
+func split(spltr Spliterator, cores_left int, find func(Spliterator)) int {
+	if cores_left > 1 {
+		var rem int = cores_left / 2
+		s := spltr.TrySplit()
+		if s != nil {
+			return split(s, rem, find) + split(spltr, rem, find)
+		} else {
+			go find(spltr)
+			return 1
+		}
+	} else {
+		go find(spltr)
+		return 1
 	}
 }
--- stream_test.go
+++ stream_test.go
@@ -7,13 +7,26 @@ import (
 )
 
 func TestStream(t *testing.T) {
-	a := stream.NewArray([]interface{}{1, 2, 3, 4, 5})
+	gen := func() *stream.Array {
+		return stream.NewArray(makeRange(1, 10))
+	}
 
-	stream.Of(a).Map(func(t op.T) op.T {
+	stream.Of(gen()).Map(func(t op.T) op.T {
 		return t.(int) * 2
 	}).Filter(func(t op.T) bool {
-		return t.(int) > 5
-	}).ForEach(func(t op.T) {
-		println(t.(int))
+		return t.(int) > 1
+	}).FindAny()
+
+	matches := stream.Of(gen()).AnyMatch(func(t op.T) bool {
+		return t.(int) > 1
 	})
+	println(matches)
+}
+
+func makeRange(min, max int) []interface{} {
+	a := make([]interface{}, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
 }
